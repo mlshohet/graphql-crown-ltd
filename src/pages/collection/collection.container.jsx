140c7fbb234e008265011d56d9b81160d1b8eec3
Mike Shohet <mlshohet@gmail.com>
2020-10-22 17:40:38 -0400

    changed collections from redux to apollo, made collections container

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -6,7 +6,7 @@ import { gql } from 'apollo-boost';
 import CollectionPage from './collection.component';
 import Spinner from '../../components/spinner/spinner.component';
 
-const GET_COLLECTION_BY_TITLE = gql`
+const GET_ONE_COLLECTION_BY_TITLE = gql`
 	query getCollectionsbyTitle($title: String!) {
 		getCollectionsbyTitle(title: $title) {
 			id
@@ -23,7 +23,7 @@ const GET_COLLECTION_BY_TITLE = gql`
 
 const CollectionPageContainer = ({ match }) => (
 		<Query
-			query={GET_COLLECTION_BY_TITLE}
+			query={GET_ONE_COLLECTION_BY_TITLE}
 			variables={{ title: match.params.collectionId }}
 		>
 			{({ loading, data }) => {
